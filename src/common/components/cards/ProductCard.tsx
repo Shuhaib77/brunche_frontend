a9484a945f2c9c5b47924c99618d17b97f9d40d9
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-12-02 08:55:11 +0530

    updated

--- src/common/components/cards/ProductCard.tsx
+++ src/common/components/cards/ProductCard.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 
-function ProductCard({ name, image, price }) {
+interface Payload {
+  name: string;
+  image: string;
+  price: number;
+}
+
+function ProductCard({ name, image, price }: Payload) {
   return (
-    <>
-      <div className="h-full w-full   ">
-        <div className="    border">
-          <img
-            src={image}
-            alt=""
-            className="w-full min-h-[900px] min-w-[500px] object-contain"
-          />
-        </div>
-        <div className="h-full">
-          <h1>{name}</h1>
-          <h1>{price}</h1>
-        </div>
+    <div className="h-full w-full">
+      <div className="border">
+        <img
+          src={image}
+          alt={name}
+          className="w-full min-h-[900px] min-w-[500px] object-contain"
+        />
       </div>
-    </>
+
+      <div className="h-full">
+        <h1>{name}</h1>
+        <h1>{price}</h1>
+      </div>
+    </div>
   );
 }
 
